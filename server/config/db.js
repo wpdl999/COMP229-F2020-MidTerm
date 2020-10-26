0d39711849990fcba87d23b1f8a7657618d7d8f0
Wooram Moon <wpdl555@naver.com>
2020-10-26 16:34:05 -0400

    GUI Design and Functionality

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://WooramM:50WqVBnda2jrKYoX@mongodbsserver.xf3dp.mongodb.net/book_store?retryWrites=true&w=majority"
+};
